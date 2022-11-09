306a6da5c6e0767e164af2408e798a752bb96e9e
Walbertty <walberttyg@gmail.com>
2022-11-09 12:50:16 -0300

    upload file

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,22 @@
+class UsersController {
+    create(request, response) {
+        const { name, email, password } = request.body
+
+        response.status(201).json({ name, email, password })
+    }
+}
+
+module.exports = UsersController
+
+
+
+
+
+
+/* Função do Controler: --- Máximo 5 funções/métodos ---
+     * index - GET para listar vários registros.
+     * show - GET para exibir um registro especifico.
+     * create - POST para criar um registro.
+     * update - PUT para atualizar um registro.
+     * delete - DELETE para remover um registro.
+*/
